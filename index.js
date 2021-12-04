5eca3262f82789c6072a365abe66304201ffc8ec
Divyanshu Dhruv <71079602+divyanshudhruv@users.noreply.github.com>
2021-12-04 21:34:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    $a = document.getElementById("mainText").value;
+    let url = "https://www.google.com/search?q=" + $a;
+    window.open(url);
+  }
+  //----------------------------------------------------
+  function myFunction2() {
+    let url = "https://play.google.com";
+    window.open(url);
+  }
+
+  function myFunction3() {
+    let url = "https://maps.google.com";
+    window.open(url);
+  }
+
+  function myFunction4() {
+    let url = "https://youtube.com";
+    window.open(url);
+  }
+
+ 
